1e99d4a042cb7b319a973405aca6f1a7a2e2e954
ZeyadWaledEbrahim <zeyadwaled165@gmail.com>
2025-04-12 19:22:00 +0200

    resolve conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import Navbar from "./components/Navbar/navBar";
 import Sidebar from "./components/Sidebar/Sidebar";
-<<<<<<< HEAD
 import ListsBoard from "./pages/ListsBoard";
 import ListTasksPage from "./pages/ListTasksPage";
 import CreateListPage from "./pages/CreateListPage";
@@ -10,17 +9,6 @@ import ManageListsPage from "./pages/ManageListsPage";
 import AddTaskModal from "./components/Navbar/addTaskModal";
 import "./App.css";
 import Table from "./components/Table/Table";
-=======
-import Table from "./components/Table/Table";
-import AddTaskModal from "./components/Navbar/addTaskModal";
-import "./App.css";
-
-const ManageTasks = () => (
-  <div className="p-4">
-    <h2>Manage Tasks</h2>
-  </div>
-);
->>>>>>> 4d9c7897a8a7a5930572acdc1a44aea59d1ab388
 
 const App = () => {
   const [lists, setLists] = useState([
@@ -81,7 +69,6 @@ const App = () => {
     setSelectedList(updatedLists.find((list) => list.id === selectedList.id));
     setShowModal(false);
     navigate("/");
-<<<<<<< HEAD
   };
 
   const addList = (newList) => {
@@ -111,44 +98,11 @@ const App = () => {
               path="/manage"
               element={<ManageListsPage lists={lists} setLists={setLists} />}
             />
-=======
-  };
-
-  return (
-    <div className="app">
-      <div className="d-flex">
-        <div className={`d-none d-lg-block ${isSidebarOpen ? "" : "d-none"}`}>
-          <Sidebar
-            isSidebarOpen={isSidebarOpen}
-            toggleSidebar={toggleSidebar}
-          />
-        </div>
-        <div className="flex-grow-1">
-          <Navbar
-            setSearchQuery={setSearchQuery}
-            handleAddTask={handleAddTask}
-            showModal={showModal}
-            setShowModal={setShowModal}
-          />
-
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <Table searchQuery={searchQuery} tasks={selectedList.tasks} />
-              }
-            />
-            <Route path="/manage" element={<ManageTasks />} />
->>>>>>> 4d9c7897a8a7a5930572acdc1a44aea59d1ab388
             <Route
               path="/create"
               element={
                 <AddTaskModal
-<<<<<<< HEAD
                   show={showModal}
-=======
-                  show={true}
->>>>>>> 4d9c7897a8a7a5930572acdc1a44aea59d1ab388
                   onClose={() => {
                     setShowModal(false);
                     navigate("/");
@@ -159,13 +113,6 @@ const App = () => {
             />
           </Routes>
         </div>
-        <button
-          className="btn btn-primary d-lg-none position-fixed"
-          style={{ bottom: "20px", left: "20px", zIndex: 1000 }}
-          onClick={toggleSidebar}
-        >
-          {isSidebarOpen ? "Hide Sidebar" : "Show Sidebar"}
-        </button>
       </div>
     </div>
   );
